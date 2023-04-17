c6718d0f29e6e02f7ab31d1e236feb94189566c3
jonginnim <jongin4546@naver.com>
2023-04-17 21:42:50 +0900

    added every stuff that i did

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,30 +3,31 @@ import java.util.Scanner;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		int num = 0;
 		Scanner scan = new Scanner(System.in);
+		FootballPlayerManager footballPlayerManager = new FootballPlayerManager(scan);
 		
-		while(num !=6 ) {
+		int num = 0;
+		while(num != 5 ) {
 			System.out.println("# Football Management System Menu #");
 			System.out.println(" 1. Add Players");
 			System.out.println(" 2. Delete Players");
 			System.out.println(" 3. Edit Players");
 			System.out.println(" 4. View Players");
-			System.out.println(" 5. Show a menu");
-			System.out.println(" 6. Exit");
-			System.out.println("Select one number between 1 - 6 : ");
+			System.out.println(" 5. Exit");
+			System.out.println("Select one number between 1 - 5 : ");
+			
 			num = scan.nextInt();
 			if (num ==1) {
-				addplayers();
+				footballPlayerManager.addplayers();
 			}
 			else if (num==2) {
-				deleteplayers();
+				footballPlayerManager.deleteplayers();
 			}
 			else if (num==3) {
-				editplayers();
+				footballPlayerManager.editplayers();
 			}
 			else if (num==4) {
-				viewplayers();
+				footballPlayerManager.viewplayers();
 			}
 			else {
 				continue;
@@ -34,34 +35,6 @@ public class MenuManager {
 	}
 
 }
-	public static void addplayers() {
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Player ID: ");
-		int playerid = scan.nextInt();
-		System.out.print("Player name: ");
-		String playername = scan.next();
-		System.out.print("SNS address: ");
-		String playerSns = scan.next();
-		System.out.print("Phone number: ");
-		String playerPhone = scan.next();
-	}
-	
-	public static void deleteplayers() {
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Player ID: ");
-		int playerid = scan.nextInt();
-	}
-	
-	public static void editplayers() {
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Player ID: ");
-		int playerid = scan.nextInt();
-	}
-	
-	public static void viewplayers() {
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Player ID: ");
-		int playerid = scan.nextInt();
-	}
+
 	
 }
